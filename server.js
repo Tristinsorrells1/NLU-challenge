7dacc207b327de5c1a8f97718f7b4f25be2e0d03
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-10-03 17:51:23 -0500

    Add post request

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ import cors from "cors";
 const app = express();
 const port = 5000;
 app.use(cors());
+app.use(express.json());
 
-const db = new sqlite3.Database("flavors.db");
+const flavorsDb = new sqlite3.Database("flavors.db");
+const contactsDb = new sqlite3.Database("contacts.db");
 
 app.get("/flavors", (req, res) => {
-  db.all("SELECT DISTINCT flavor FROM flavors", (err, rows) => {
+  flavorsDb.all("SELECT DISTINCT flavor FROM flavors", (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
       return;
@@ -20,7 +22,7 @@ app.get("/flavors", (req, res) => {
 });
 
 app.get("/categories", (req, res) => {
-  db.all("SELECT DISTINCT category FROM flavors", (err, rows) => {
+  flavorsDb.all("SELECT DISTINCT category FROM flavors", (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
       return;
@@ -34,7 +36,7 @@ app.get("/categories", (req, res) => {
 app.get("/flavors/:category", (req, res) => {
   const category = req.params.category;
 
-  db.all("SELECT * FROM flavors WHERE category = ?", [category], (err, rows) => {
+  flavorsDb.all("SELECT * FROM flavors WHERE category = ?", [category], (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
       return;
@@ -43,6 +45,31 @@ app.get("/flavors/:category", (req, res) => {
     res.json({ data: rows });
   });
 });
+
+app.post("/contacts", (req, res) => {
+ 
+  const formData = req.body;
+
+  const { capability, email, message, name, sendNewsletter } = formData;
+
+  const insertQuery = `
+    INSERT INTO contacts (capability, email, message, name, sendNewsletter)
+    VALUES (?, ?, ?, ?, ?)
+  `;
+
+  contactsDb.run(insertQuery, [capability, email, message, name, sendNewsletter], function (err) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: "New contact added successfully",
+      contactId: this.lastID,
+    });
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
